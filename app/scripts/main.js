49d91de89c5e4fd937cadca1d36f9bc3cc1b718c
Brian J. Cardiff <bcardiff@manas.com.ar>
2015-12-29 18:28:33 -0300

    interact with split graphs. draw round result and assignments

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,13 +10,15 @@ var s = {
     myTargetSalary: [{x:0,y:28000},{x:90,y:10000}],
   },
 
+  progress: [.2, .3, .4, .1],
+
   salaryIncrease: 8000,
 
   maxOrder: 100,
   minSalary: 10000, maxSalary: 30000,
 
   width:  900,
-  height: 450,
+  height: 250,
 
   splitHeight: 250,
   splitWidth:  400,
@@ -25,6 +27,22 @@ var s = {
   splitMaxSalary: 25000,
   splitMinSalary: 12000,
 }
+
+_.each(s.data, function(d) {
+  if (!d.assignment) {
+    d.assignment = [];
+    d.roundResult = [];
+    _.each(s.progress, function() {
+      if (d.name == 'John') {
+        d.assignment.push(0);
+        d.roundResult.push(0);
+      } else {
+        d.assignment.push(Math.round(Math.random() * 200));
+        d.roundResult.push(Math.round(Math.random() * 200));
+      }
+    });
+  }
+});
 
 var m = [15, 15, 50, 60]; // margins
 var sm = [10, 10, 30, 60]; // margins
@@ -131,31 +149,53 @@ _.each(_.chunk(names, s.splitRowCount), function(rowNames) {
     var initialSalary = svg.append("circle").attr("r", 5);
     var targetSalary = svg.append("circle").attr("class", "split-target").attr("r", 5);
 
-    splitCharts[name] = {
-      svg : svg,
-      updateTargetSalary: function(data) {
-        var splitSalaryScale = d3.scale.linear().domain([data.myTargetSalary, data.salary]).range([sm[0], s.splitHeight - sm[2]]);
+    var roundResultSelection = svg.append("g").attr("class", "split-round-result").selectAll("circle").data(s.progress)
+      .enter().append("circle").attr("r", 5)
+      .attr("cx", function(d, i) { return splitProgressScale(_.sum(_.slice(s.progress, 0, i+1))); });
 
-        salaryAxis.attr("transform", "translate(" + splitProgressScale(0) + ",0)").call(
-          d3.svg.axis().scale(splitSalaryScale).orient("left").ticks(10)
-        );
+    var assignmentSelection = svg.append("g").attr("class", "split-round-assignment").selectAll("circle").data(s.progress)
+      .enter().append("circle").attr("r", 5)
+      .attr("cx", function(d, i) { return splitProgressScale(_.sum(_.slice(s.progress, 0, i+1))); })
+      .call(d3.behavior.drag().on("drag", function(d, i) {
+        obj.data.assignment[i] = Math.round(obj.splitSalaryScale.invert(d3.event.y)) - obj.baseSalaryForAssignment(obj.data, i);
+        updateGraph();
+      }));
 
-        initialSalary
-          .attr("cy", splitSalaryScale(data.salary))
-          .attr("cx", splitProgressScale(0.0));
+    var obj = splitCharts[name] = {};
+    obj.svg = svg;
+    obj.updateGraph = function(data) {
+      obj.data = data;
+      obj.splitSalaryScale = d3.scale.linear().domain([data.myTargetSalary + (data.myTargetSalary-data.salary) * .3, data.salary]).range([sm[0], s.splitHeight - sm[2]]);
 
-        targetSalary
-          .attr("cy", splitSalaryScale(data.myTargetSalary))
-          .attr("cx", splitProgressScale(1.0));
+      salaryAxis.attr("transform", "translate(" + splitProgressScale(0) + ",0)").call(
+        d3.svg.axis().scale(obj.splitSalaryScale).orient("left").ticks(10)
+      );
 
-        targetRef
-          .attr("x1", splitProgressScale(0.0))
-          .attr("y1", splitSalaryScale(data.salary))
-          .attr("x2", splitProgressScale(1.0))
-          .attr("y2", splitSalaryScale(data.myTargetSalary));
+      initialSalary
+        .attr("cy", obj.splitSalaryScale(data.salary))
+        .attr("cx", splitProgressScale(0.0));
 
-      },
-    }
+      targetSalary
+        .attr("cy", obj.splitSalaryScale(data.myTargetSalary))
+        .attr("cx", splitProgressScale(1.0));
+
+      targetRef
+        .attr("x1", splitProgressScale(0.0))
+        .attr("y1", obj.splitSalaryScale(data.salary))
+        .attr("x2", splitProgressScale(1.0))
+        .attr("y2", obj.splitSalaryScale(data.myTargetSalary));
+
+      assignmentSelection
+        .attr("cy", function(d, i) { return obj.splitSalaryScale(obj.baseSalaryForAssignment(data, i) + data.assignment[i]); } )
+
+      roundResultSelection
+        .attr("cy", function(d, i) { return obj.splitSalaryScale(obj.baseSalaryForAssignment(data, i-1) + data.roundResult[i]); } )
+    };
+
+    obj.baseSalaryForAssignment = function(data, i) {
+      return data.salary + _.sum(_.slice(data.roundResult, 0, i+1));
+    };
+
   });
 });
 
@@ -183,7 +223,7 @@ function updateGraph() {
 
 
   _.each(s.data, function(data) {
-    splitCharts[data.name].updateTargetSalary(data);
+    splitCharts[data.name].updateGraph(data);
   });
 }
 
